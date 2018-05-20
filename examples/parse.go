70d03a8379d77a2fafd32871fd5553089dcba8dd
Julien Vehent <julien@linuxwall.info>
2018-05-20 12:57:52 -0400

    add signature block marshalling

--- examples/parse.go
+++ examples/parse.go
@@ -1,7 +1,11 @@
 package main
 
 import (
-	"encoding/json"
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha512"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -20,9 +24,34 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	jsonOut, err := json.MarshalIndent(file, "", "    ")
+	signedBlock, err := file.MarshalForSignature()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", jsonOut)
+	hashed := sha512.Sum384(signedBlock)
+	foundValidSig := false
+	for i, sig := range file.Signatures {
+		for keyName, keyPem := range mar.FirefoxReleasePublicKeys {
+			block, _ := pem.Decode([]byte(keyPem))
+			if block == nil {
+				log.Fatal("failed to parse PEM block containing the public key")
+			}
+
+			pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+			if err != nil {
+				log.Fatal("failed to parse DER encoded public key: " + err.Error())
+			}
+			rsaPub := pub.(*rsa.PublicKey)
+			err = rsa.VerifyPKCS1v15(rsaPub, crypto.SHA384, hashed[:], sig.Data)
+			if err == nil {
+				fmt.Printf("* %20s\t(rsa %d bits): pass signature %d\n", keyName, rsaPub.N.BitLen(), i)
+				foundValidSig = true
+			} else {
+				fmt.Printf("* %20s\t(rsa %d bits): failed signature %d\n", keyName, rsaPub.N.BitLen(), i)
+			}
+		}
+	}
+	if !foundValidSig {
+		log.Fatal("Signature: failed")
+	}
 }
